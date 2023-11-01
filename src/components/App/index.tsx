2261ad912b347394c08b82832bcb415e989cd26c
ifb24m <fakem1ner@yandex.ru>
2023-11-01 06:47:37 +0400

    added brand info loading from api

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,15 +1,11 @@
 // 'use client';
 
-import { ReactNode } from 'react';
+// import { ReactNode } from 'react';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+// import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-const queryClient = new QueryClient();
-
-export const App = ({ children }: { children: ReactNode }): ReactNode => {
-	return (
-		<QueryClientProvider client={JSON.parse(JSON.stringify(queryClient))}>
-			{children}
-		</QueryClientProvider>
-	);
-};
+// export const App = ({ children }: { children: ReactNode }): ReactNode => {
+// 	return (
+		
+// 	);
+// };
